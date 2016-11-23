62cd6fb4cc0366a44a1eebdb6a58130557a2d77b
chaiyanpu <chaiyanpu@126.com>
2016-11-23 19:52:41 +0800

    get deep of a binary tree
    get deep of a binary tree

--- BinaryTree/BinaryTree-c:c++/BinaryTree/main.cpp
+++ BinaryTree/BinaryTree-c:c++/BinaryTree/main.cpp
@@ -12,38 +12,38 @@
 //using namespace std;
 
 ///二叉树的结构
-struct BinaryTreenode{
+struct BinaryTreeNode{
     int value;
-    BinaryTreenode *pLeft = NULL;
-    BinaryTreenode *pRight = NULL;
+    BinaryTreeNode *left = NULL;
+    BinaryTreeNode *right = NULL;
 };
 
-BinaryTreenode * buildBinaryTreenodeFunc(
+BinaryTreeNode * buildBinaryTreeNodeFunc(
                                          int * startPreorder,
                                          int * endPreorder,
                                          int * startInorder,
                                          int * endInorder );
 
 ///根据前序遍历和中序便利重建二叉树
-BinaryTreenode * buildBinaryTreenode(int * preorder,int * inorder,int length){
+BinaryTreeNode * buildBinaryTreeNode(int * preorder,int * inorder,int length){
     
     if (preorder == NULL || inorder == NULL || length < 0){
         return NULL;
     }
     
-    return buildBinaryTreenodeFunc(preorder,
+    return buildBinaryTreeNodeFunc(preorder,
                                    preorder + length - 1,
                                    inorder,
                                    inorder + length - 1);
 }
 
-BinaryTreenode * buildBinaryTreenodeFunc(
+BinaryTreeNode * buildBinaryTreeNodeFunc(
                                          int * startPreorder,
                                          int * endPreorder,
                                          int * startInorder,
                                          int * endInorder ){
     int rootValue = startPreorder[0];//根节点值
-    BinaryTreenode * root = new BinaryTreenode();
+    BinaryTreeNode * root = new BinaryTreeNode();
     root->value = rootValue;
     
     if (startPreorder == endInorder){  //只有根节点
@@ -68,14 +68,14 @@ BinaryTreenode * buildBinaryTreenodeFunc(
     
     if (length > 0){
         //构建左子树
-        root->pLeft = buildBinaryTreenodeFunc(startPreorder + 1,
+        root->left = buildBinaryTreeNodeFunc(startPreorder + 1,
                                               startPreorder + length,
                                               startInorder,
                                               startInorder + length - 1);
     }
     if (length < endInorder - startInorder){
         //构建右子树
-        root->pRight = buildBinaryTreenodeFunc(startPreorder + length + 1,
+        root->right = buildBinaryTreeNodeFunc(startPreorder + length + 1,
                                                endPreorder,
                                                startInorder + length + 1,
                                                endInorder);//这个的范围应该是一样的
@@ -86,22 +86,31 @@ BinaryTreenode * buildBinaryTreenodeFunc(
 
 //MARK:前序遍历二叉树
 ///前序遍历二叉树
-void preorderBinaryTree(BinaryTreenode * tree){
+void preorderBinaryTree(BinaryTreeNode * tree){
     
-    BinaryTreenode * node = tree;
-    std::stack<BinaryTreenode *> aStack;
+    BinaryTreeNode * node = tree;
+    std::stack<BinaryTreeNode *> aStack;
 
     while (aStack.size() > 0 || node != NULL) {
         if (node != NULL){
             printf("%d",node->value);
             aStack.push(node);
-            node = node->pLeft;
+            node = node->left;
         }else{
-            node = (aStack.top())->pRight;
+            node = (aStack.top())->right;
             aStack.pop();
         }
     }
+}
 
+/// get deep length of a binary tree.
+int getDeepWithABinaryTree(BinaryTreeNode * tree){
+    if (tree == NULL){
+        return  0;
+    }
+    int leftLen = getDeepWithABinaryTree(tree->left);
+    int rightLen = getDeepWithABinaryTree(tree->right);
+    return leftLen > rightLen ? leftLen + 1 : rightLen + 1;
 }
 
 
@@ -111,16 +120,19 @@ int main(int argc, const char * argv[]) {
     int length = 8;
     int preorder[8] = {1,2,4,7,3,5,6,8};
     int inorder[8] = {4,7,2,1,5,3,8,6};
-    BinaryTreenode * binaryTreenode = buildBinaryTreenode(preorder, inorder, length);
+    printf("build binary tree\n");
+    BinaryTreeNode * binaryTreeNode = buildBinaryTreeNode(preorder, inorder, length);
     
-    printf("%d------",binaryTreenode->pLeft->value);
-    printf("%d------",binaryTreenode->pLeft->pLeft->value);
-    printf("%d------",binaryTreenode->pLeft->pLeft->pRight->value);
-    
-    
+    printf("%d------",binaryTreeNode->left->value);
+    printf("%d------",binaryTreeNode->left->left->value);
+    printf("%d------\n",binaryTreeNode->left->left->right->value);
+    printf("\n");
     //前序遍历二叉树
-    preorderBinaryTree(binaryTreenode);
-    
+    printf("preorder binary tree:\n");
+    preorderBinaryTree(binaryTreeNode);
+     printf("\n");printf("\n");
+    //得到二叉树的深度
+    printf("the deep of binary tree is %d\n",getDeepWithABinaryTree(binaryTreeNode));
     
     return 0;
 }
---
+++ DeepOfBinaryTree/DeepOfBinaryTreeC++/DeepOfBinaryTreeC++/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  DeepOfBinaryTreeC++
+//
+//  Created by chai on 2016/11/23.
+//  Copyright © 2016年 chaiyanpu. All rights reserved.
+
+//  get deep length of a binary tree.
+
+#include <iostream>
+
+struct BinaryNode{
+    int value;
+    BinaryNode * left;
+    BinaryNode * right;
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
