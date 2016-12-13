301fd48e076be8c30b7409a3982bf44d9db447dd
chaiyanpu <chaiyanpu@126.com>
2016-12-13 21:48:33 +0800

    the least k numbers in an array Swift
    the least k numbers in an array Swift

---
+++ Array/MinNumbersOfArray/MinNumbersOfArraySwift/MinNumbersOfArraySwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MinNumbersOfArraySwift
+//
+//  Created by chai on 2016/12/13.
+//  Copyright © 2016年 chaiyanpu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
