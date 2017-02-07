d7cfe6afc87a6f621e5706261999aed8562957e7
chaiyanpu <chaiyanpu@126.com>
2017-02-07 20:30:32 +0800

    leetCode Add Two Numbers
    leetCode Add Two Numbers

--- SwiftLeetCode/LeetCode1-5/LeetCode1-5/main.swift
+++ SwiftLeetCode/LeetCode1-5/LeetCode1-5/main.swift
@@ -15,6 +15,7 @@ print("Hello, World!")
     Given an array of integers, return indices of the two numbers such that they add up to a specific target.
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
     A:
+    time:O(n)
     利用字典，将数组的值做为key，index做为value。根据需要的目标值，在字典中查询是否存在目标，返回结果。
  */
 class TwoSum {
@@ -43,6 +44,7 @@ class TwoSum {
  
     You may assume the two numbers do not contain any leading zero, except the number 0 itself.
     A:
+    time:O(n)
     两个指针同时往下走，将链表节点的值相加，添加入新的链表，判断是否需要进位，直到节点遍历完毕且不需要进位为止。
  */
 //list
@@ -84,5 +86,57 @@ class AddTwoNumbers {
         return head
     }
 }
+
+
+/* 3.Add Two Numbers
+ Q:
+ Given a string, find the length of the longest substring without repeating characters.
+ A:
+ 使用一个哈希表，记录字符的索引。遍历，当检测到相同的时候，并且将字典中的重复之前的包括第一个重复的字符的值设为false，求出不重复的length，将length记录下来并与max比较。
+ */
+class LengthOfLongestSubstring {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var max:Int = 0
+        var start:Int = 0
+        var end:Int = 0
+        var dic:[Character:Bool] = [:]
+        for c in s.characters {
+            if dic[c] == nil || dic[c] == false {
+                dic[c] = true;//遍历标记
+                end += 1;
+            }else{
+                let index = s.index(s.startIndex, offsetBy: start)
+                while s[index] != c {
+                    dic[s[index]] = false //表示已经遍历并被计算过的，其实没有作用
+                    start += 1;
+                }
+                start += 1
+                end += 1
+            }
+            max = max > end - start ? max : end - start
+        }
+        return max
+    }
+}
+
+
+/* 4.Median of Two Sorted Arrays
+ Q:
+ There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ 
+ Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ A:
+
+ */
+class MedianOfTwoSortedArrays{
+    func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
+        
+        return 0.1
+    }
+}
+
+
+
+
 
 
