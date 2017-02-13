812cf1090a39afc7c97e7917029643bc4506fe19
chaiyanpu <chaiyanpu@126.com>
2017-02-13 19:25:45 +0800

    leetCode6-10 start
    leetCode6-10 start

---
+++ SwiftLeetCode6-10/SwiftLeetCode6-10/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLeetCode6-10
+//
+//  Created by chai on 2017/2/13.
+//  Copyright © 2017年 chaiyanpu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
